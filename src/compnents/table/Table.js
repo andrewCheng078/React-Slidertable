e65f8f0ab50c625e7dfa7294a9f7f7030146f6b1
andrew <andrewCheng@gmail.com>
2019-07-10 18:00:17 +0800

    7/10 test table item active class

--- src/compnents/table/Table.js
+++ src/compnents/table/Table.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './Table.scss';
 import json from '../../ticketInfo';
 console.log('json',json);
+
 export default class Table extends Component {
     constructor(){
         super();
         this.state={
             show:'3',
-            moveRight:'1',
+            moveRight:'3',
             slide:'3',
             tableData:[],
             n:0,
             R_ButtonHide:true,
             L_ButtonHide:false,
+            moveClass:'',
+            activeClass:false,
         }
         
     }
@@ -28,14 +31,23 @@ export default class Table extends Component {
         if(this.state.n === '-100%'){this.setState({n:'0%', L_ButtonHide:false,R_ButtonHide:true})}
        
     }
+    activeClass(e){
+        console.log(e.target)
+        //加上一個class變成active
+        //計算餘數
+        //
+        // this.setState({
+        //     activeClass:!this.activeClass
+        // })
 
+    }
     componentDidMount(){
 
         this.setState({tableData:json.data[0].data,});    
 
     }
     render() {
-        const { show,moveRight,n,tableData,R_ButtonHide,L_ButtonHide } = this.state;
+        const { show,moveRight,n,tableData,R_ButtonHide,L_ButtonHide,moveClass,activeClass } = this.state;
         return (
         <div className="table">
             <button className={`m-Button m-rightButton ${R_ButtonHide?` `:`buttonHide`}`} onClick={this.tableRight.bind(this)}>{'>'}</button>
@@ -50,7 +62,7 @@ export default class Table extends Component {
                                     </div>
                                 </td>
                                 <td className="hidden">
-                                    <div className={`nowrap moveRight-${moveRight}`} style={{transform: `translateX(${n})`}}>
+                                    <div className={`nowrap moveRight-${moveRight}  ${ moveClass }`}  style={{transform: `translateX(${n})`}}>
                                         <div className={`navDate-bg show-${show}`}><span>12/30(六)</span></div>
                                         <div className={`navDate-bg show-${show}`}><span>12/31(日)</span></div>
                                         <div className={`navDate-bg show-${show}`}><span>1/1(一)</span></div>
@@ -62,19 +74,22 @@ export default class Table extends Component {
                                 </td>
                             </tr>
 
-                        {tableData.map((data,index)=>{
-                            return    <tr key={`${ data.goDate }+${ index }`}>
+                        {tableData.map((data,index1)=>{
+                            return    <tr key={`${ data.goDate }+${ index1 }`}>
                                         <td className="navDate-title-bg">
                                             <div>
                                                 <span>{ data.goDate }</span>
                                             </div>
                                         </td>
                                         <td className="hidden">
-                                            <div className={ `nowrap moveRight-${ moveRight }` } style={{transform: `translateX(${n})`}}>
-                                                {data.detail.map((detail,index)=>{
-                                                    return  <div key={`${detail.backDate}+${detail.price}+${index}`} className={`show-${ show } ${detail.cheapest?'cheapest':null}`  }>
+                                            <div className={ `nowrap moveRight-${ moveRight } ${ moveClass }` } style={{transform: `translateX(${ n })`}}>
+                                                {data.detail.map((detail,index2)=>{
+                                                    return  <div key={`${ detail.backDate }+${ detail.price }+${ index2 }`} id={ (index1*7)+index2 } 
+                                                                className={`show-${ show } ${ detail.cheapest ? 'cheapest' : '' } ${activeClass?'active':''}` }
+                                                                onClick={this.activeClass.bind(this)}
+                                                                >
                                                                 <span>
-                                                                    { detail.price === "--"?'查看':(detail.price === " "?"--":detail.price +' 起') }
+                                                                    { detail.price === "--" ? '查看' : (detail.price === " " ? "--" : detail.price +' 起') }
                                                                 </span>
                                                             </div>
                                                 })}
